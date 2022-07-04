db1b69e39d26c226ecf1c3db3803047d14dafcf4
formegusto <formegusto@gmail.com>
2022-07-04 17:10:12 +0900

    Component Theory #01State

--- 1#ComponentTheory/01#state/script/index.js
+++ 1#ComponentTheory/01#state/script/index.js
@@ -1,26 +1,10 @@
-const $app = document.getElementById("app");
+import Items from "./components/Items";
 
-let state = {
-  items: ["item1", "item2", "item3", "item4"],
-};
+class App {
+  constructor() {
+    const $app = document.getElementById("app");
+    new Items($app);
+  }
+}
 
-const render = () => {
-  const { items } = state;
-  $app.innerHTML = `
-    <ul>
-        ${items.map((item) => `<li>${item}</li>`).join("")}
-    </ul>
-    <button id="append">추가</button>
-  `;
-
-  document.querySelector("#append").addEventListener("click", () => {
-    setState({ items: [...items, `item${items.length + 1}`] });
-  });
-};
-
-const setState = (newState) => {
-  state = { ...state, ...newState };
-  render();
-};
-
-render();
+new App();
